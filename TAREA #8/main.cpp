12f54330d946f23170548a9c020e1b18d5cfd35d
KALF9912 <100754154+KALF9912@users.noreply.github.com>
2022-03-23 22:31:50 -0600

    Add files via upload

---
+++ TAREA #8/main.cpp
@@ -0,0 +1,24 @@
+# incluir  < iostream >
+# incluir  < matemáticas.h >
+
+utilice el espacio de nombres estandar;
+
+int  principal ()
+{
+    float num= 0 , rad = 2 , resultado = 0 ;
+    
+	// Proceso
+    
+    cout << " Ingrese el digito al cual desea calcular la raiz: " << endl;
+    cin >> numero;
+    resultado = pow (num,( 1 /rad));
+    sistema ( " color 08 " );
+    cout << "  El resultado de la raiz cuadrada es " << num << " Es : " << resultado << endl;
+    
+    devolver  0 ;
+}
+
+
+int main(int argc, char** argv) {
+	return 0;
+}
